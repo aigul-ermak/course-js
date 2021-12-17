4907b0719ba9d3dbf90924759c2ee960b5915064
aigul <aigul.erma@gmail.com>
2021-12-17 12:24:26 -0400

    tasks

--- js/script.js
+++ js/script.js
@@ -1,22 +1,45 @@
-"use strict";
+'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let num = 50;
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-// while (num < 55) {
-// 	console.log(num);
-// 	num++;
-// }
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-// for ( let i = 1; i < 10; i++) {
-// 	console.log(num);
-// 	num ++;
-// }
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-for (let i = 1; i < 10; i++) {
-	if (i === 6) {
-		// break;
-		continue;
-	}
-	console.log(i);
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+		b = prompt('На сколько оцените его?', '');
+
+		if (a != null && b!= null && a != "" && b != "" && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('yes');
+		} else {
+			console.log('error');
+			i--;
+		}
+
+		if (personalMovieDB.count < 10 ) {
+			console.log('not to much');
+		} else if ( personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+			console.log('you are classic');
+		} else {
+			console.log('error');
+		}
+	
 }
+
+
+
+console.log(personalMovieDB);
