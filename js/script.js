cded5c4b6ecc44783c4d84030c9864a7b52d6469
aigul <aigul.erma@gmail.com>
2021-12-17 11:59:42 -0400

    while for cycyles

--- js/script.js
+++ js/script.js
@@ -1,40 +1,22 @@
 "use strict";
 
-if (4 == 9) {
-	console.log('ok');
-} else {
-	console.log('error');
-}
 
-// const num = 50;
+let num = 50;
 
-// if (num < 49) {
-// 	console.log('error');
-// } else if (num > 100) {
-// 	console.log('too much');
-// } else {
-// 	console.log('ok');
+// while (num < 55) {
+// 	console.log(num);
+// 	num++;
 // }
 
+// for ( let i = 1; i < 10; i++) {
+// 	console.log(num);
+// 	num ++;
+// }
 
-// const num = 50;
-// (num == 50) ? console.log('ok'): console.log('error');
-
-const num = 50;
-switch (num) {
-	case 49:
-		console.log('no');
-		break;
-
-	case 45:
-		console.log('no');
-		break;
-
-	case 50:
-		console.log('yes');
-		break;
-
-	default:
-		console.log('next time lucky');
-		break;
+for (let i = 1; i < 10; i++) {
+	if (i === 6) {
+		// break;
+		continue;
+	}
+	console.log(i);
 }
