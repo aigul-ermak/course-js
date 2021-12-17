da38aa44535e4bc231fed903250c0a550ca1aa21
aigul <aigul.erma@gmail.com>
2021-12-17 14:52:01 -0400

    function

--- js/script.js
+++ js/script.js
@@ -1,45 +1,19 @@
 'use strict';
+console.log(calc(5, 2));
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt('На сколько оцените его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов?', ''),
-//       d = prompt('На сколько оцените его?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-for (let i = 0; i < 2; i++) {
-	const a = prompt('Один из последних просмотренных фильмов?', ''),
-		b = prompt('На сколько оцените его?', '');
-
-		if (a != null && b!= null && a != "" && b != "" && a.length < 50) {
-			personalMovieDB.movies[a] = b;
-			console.log('yes');
-		} else {
-			console.log('error');
-			i--;
-		}
-
-		if (personalMovieDB.count < 10 ) {
-			console.log('not to much');
-		} else if ( personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-			console.log('you are classic');
-		} else {
-			console.log('error');
-		}
-	
+function calc(a, b) {
+	return (a + b);
 }
 
 
 
-console.log(personalMovieDB);
+
+function ret() {
+	let num = 50;
+	return num;
+}
+
+console.log(ret());
+
+const anotherNum = ret()
+console.log(anotherNum);
