33979728bf9a46190cd925f866bb007383696008
aigul <aigul.erma@gmail.com>
2021-12-17 16:14:50 -0400

    function tasks

--- js/script.js
+++ js/script.js
@@ -1,19 +1,74 @@
 'use strict';
-console.log(calc(5, 2));
 
-function calc(a, b) {
-	return (a + b);
+let numberOfFilms;
+
+function start() {
+	let numberOfFilms = +prompt('How many movies did you see?', '');
+
+	while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('How many movies did you see?', '');
+	}
+}
+start();
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: true
+};
+
+
+
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('What movie did you see tha last?', ''),
+			b = prompt('Give access?', '');
+
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+		} else {
+			console.log('error');
+			i--;
+		}
+	}
 }
 
+// rememberMyFilms();
 
-
-
-function ret() {
-	let num = 50;
-	return num;
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log("How many movies?");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log("You are classic");
+	} else if (personalMovieDB.count >= 30) {
+		console.log("You are kinoman");
+	} else {
+		console.log("Error");
+	}
 }
 
-console.log(ret());
+// detectPersonalLevel ();
 
-const anotherNum = ret()
-console.log(anotherNum);
+function showMyDb(hidden) {
+	// let hidden = personalMovieDB.privat
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDb(personalMovieDB.privat);
+
+function writeYourGenres () {
+	for(let i = 1; i <= 3; i ++) {
+		// const genre = prompt(`Your favorite ${i}`)
+		// personalMovieDB.genres[i - 1] = genre;
+
+		personalMovieDB.genres[i - 1] = prompt(`Your favorite ${i}`)
+	}
+}
+		writeYourGenres();
+
+console.log(personalMovieDB);
