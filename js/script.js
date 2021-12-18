afd36f5ca0d4f2cccbd73f48a0b0336b39cd1cb2
aigul <aigul.erma@gmail.com>
2021-12-18 13:35:20 -0400

    spread operator

--- js/script.js
+++ js/script.js
@@ -1,21 +1,76 @@
 'use strict';
 
-const arr = [1, 2, 3, 4, 5];
+// let a = 5;
+// b = a;
 
-arr.forEach((element, i, arr) => {
-console.log(` ${i}: ${element} in ${arr}`);
-});
+// b = b + 5;
 
+// console.log(b);
+// console.log(a);
 
-// arr.pop();
-// arr.push(10)
-
-// console.log(arr);
-
-// for (let i = 0; i < arr.length; i ++) {
-// 	console.log(arr[i]);
+// const obj = {
+// 	a: 5,
+// 	b: 1
 // }
 
-// for( let value of arr) {
-// 	console.log(value);
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy(mainObj) {
+// 	let objCopy = {};
+// 	let key;
+// 	for (key in mainObj) {
+// 		objCopy[key] = mainObj[key];
+// 	}
+// 	return objCopy;
 // }
+
+// const numbers = {
+// 	a: 2,
+// 	b: 3,
+// 	c: {
+// 		x: 7,
+// 		y: 10
+// 	}
+// }
+
+// const newNumbers = copy(numbers);
+// newNumbers.a = 10;
+// newNumbers.c.x = 15;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+// 	d: 17,
+// 	i: 20
+// }
+
+// console.log(Object.assign(numbers, add));
+
+// const oldArrat = ['a', 'b', 'c'];
+// const newArray = oldArrat.slice();
+// newArray[1] [ 'jkhfk']
+// console.log(oldArrat);
+// console.log(newArray);
+
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newArr = [...array];
+
+console.log(newArr);
+
