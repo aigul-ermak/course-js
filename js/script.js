bddae20ae052d74ec5146d911936d7a0c9162ac4
aigul <aigul.erma@gmail.com>
2021-12-19 18:31:43 -0400

    get elements

--- js/script.js
+++ js/script.js
@@ -1,23 +1,53 @@
 'use strict';
 
-const box = document.getElementById('box');
-console.log(box);
+const box = document.getElementById('box'),
+	btns = document.getElementsByTagName('button'),
+	circles = document.getElementsByClassName('circle'),
+	wrapper = document.querySelector('.wrapper'),
+	hearts = wrapper.querySelectorAll('.heart'),
+	btn = document.querySelector('button');
 
-const btns = document.getElementsByTagName('button');
-console.log(btns[1]);
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+	// box.style.backgroundColor = 'blue';
+	// box.style.width = '500px';
 
-const hearts = document.querySelectorAll('.heart');
+	box.style.cssText = `background-color: blue; width: 500px`;
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+for(let i = 0; i < hearts.length; i ++) {
+	hearts[i].style.backgroundColor = 'blue';
+}
 
 hearts.forEach(item => {
-	console.log(item);
-})
-// console.log(hearts);
+	item.style.backgroundColor = 'blue';
+});
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('Text Text')
 
-const btn = document.querySelector('button');
-console.log(btn);
+div.classList.add('black');
+
+// document.body.append(div);
+// wrapper.append(div);
+wrapper.appendChild(div);
+// wrapper.prepend(div);
+
+// wrapper.insertBefore(div, hearts[2]);
+
+// hearts[0].before(div);
+// hearts[0].after(div);
+
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]);
+
+
+// hearts[0].replaceWith(circles[0]);
+wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hello</h1>";
+
+// div.textContent = "Hi!"//only text
+
+div.insertAdjacentHTML('beforeend', '<h2>Good day!</h2>')
