dc1066e48c30358a0e698836d20cd48fd48e3386
aigul <aigul.erma@gmail.com>
2021-12-25 10:43:22 -0400

    lesson 44 modal window

--- dist/js/script.js
+++ dist/js/script.js
@@ -98,30 +98,47 @@ window.addEventListener('DOMContentLoaded', () => {
 		modal = document.querySelector('.modal'),
 		modalCloseBtn = document.querySelector('[data-close]');
 
-modalTrigger.forEach(btn => {
-	btn.addEventListener('click', () => {
+	function openModal() {
 		modal.classList.add('show');
 		modal.classList.remove('hide');
 		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', openModal);
 	});
-});
-function closeModal() {
-	modal.classList.add('hide');
-	modal.classList.remove('show');
-	document.body.style.overflow = '';
-}
 
-		modalCloseBtn.addEventListener('click', closeModal);
+	function closeModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show');
+		document.body.style.overflow = '';
+	}
 
-		modal.addEventListener('click', (e) => {
-			if(e.target === modal) {
-				closeModal();
-			}
-		})
+	modalCloseBtn.addEventListener('click', closeModal);
 
-		document.addEventListener('keydown', (e) => {
-			if ( e.code === 'Escape' && modal.classList.contains('show')) {
-				closeModal();
-			}
-		})
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			closeModal();
+		}
+	})
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code === 'Escape' && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	const modalTimerId = setTimeout(openModal, 3000);
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >=
+			document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll)
+		}
+	};
+
+	window.addEventListener('scroll', showModalByScroll);
+	
 });
