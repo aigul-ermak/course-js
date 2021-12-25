597521cb59077239bf35bf69f4f61800590784d8
aigul <aigul.erma@gmail.com>
2021-12-25 12:15:38 -0400

    lesson 46 this

--- js/script.js
+++ js/script.js
@@ -1,32 +1,95 @@
 'use strict';
 
-// const num = new Number(2)
-// console.log(num);
+// function showThis(a, b) {
+// 	console.log(this); //будет ссылаться на глобальный объект window
+// 	function sum() {
+// 		console.log(this);
+// 		return a + b
+// 	}
+// 	console.log(sum());
+// };
 
-// const num = new Function(2)
-// console.log(num);
+// showThis(4, 5);
 
-function User (name, id) {
-	this.name = name;
-	this.id = id;
-	this.human = true;
-	this.hello = function () {
-		console.log(`Hello ${this.name}`);
-	};
+// const obj = {
+// 	a: 20,
+// 	b: 15,
+// 	sum: function () {
+// 		console.log(this);
+// 	}
+// };
+// obj.sum();
 
-}
+// function User(name, id) {
+// 	this.name = name;
+// 	this.id = id;
+// 	this.human = true;
+// }
+// let ivan = new User('Ivan', 32)
 
-User.prototype.exit = function () {
-	console.log(`User ${this.name} go out`);
-};
+// function sayName (surname) {
+// 	console.log(this);
+// 	console.log(this.name);
+// }
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 35);
+// const user = {
+// 	name: 'John'
+// }
 
-ivan.exit();
+// sayName.call(user);
+// sayName.apply(user);
 
-// ivan.hello();
-// alex.hello();
+// function sayName (surname) {
+// 	console.log(this);
+// 	console.log(this.name + surname);
+// }
 
-// console.log(ivan);
-// console.log(alex);
+// const user = {
+// 	name: 'John'
+// }
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);// здесь мы просто установили контекст
+
+// function count(num) {
+// 	return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+
+
+
+//1 обычная функция : this = window, if use strict = undefined
+//2 если внутри объекта будем использовать метод то контекст вызова будет ссылаться на этот объект
+//контекст у методов объекта будет сам объект
+//3 this в конструкторах и классах это новый экземпляр объекта
+//4 ручная привязка this - call apply bind
+
+const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function() {
+// 	console.log(this);//контекстом вызова будет сам элемент на котором произошло событие - если записано как класс функ
+// })
+
+btn.addEventListener('click', function() {
+	this.style.backgroundColor = 'red';
+})
+
+// btn.addEventListener('click', () => {
+// 	this.style.backgroundColor = 'red';//контекстом вызова будет сам элемент на котором произошло событие - если записано как класс функ
+// })
+
+// const obj = {
+// 	num: 5,
+// 	sayNumber : function() {
+// 		const say = () => {
+// 			console.log(this);
+// 		};
+// 		say();
+// 	}
+// }
+// obj.sayNumber();
+
+// const double = a => a * 2;
+// console.log(double(4));
