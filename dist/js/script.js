4df7dbb10d2da7e8e608e1b9a86fd1a34fa86128
aigul <aigul.erma@gmail.com>
2021-12-29 22:06:30 -0400

    axios

--- dist/js/script.js
+++ dist/js/script.js
@@ -191,12 +191,25 @@ window.addEventListener('DOMContentLoaded', () => {
 		return await res.json();
 	}
 
-	getResource('http://localhost:3000/menu')
-	.then(data => {
-		data.forEach(({img, altimg, title, descr, price}) => {
-			new MenuCard(img, altimg, title, descr, price,'.menu .container').render();
+	axios.get('http://localhost:3000/menu')
+		.then(data => {
+			data.data.forEach(({
+				img,
+				altimg,
+				title,
+				descr,
+				price
+			}) => {
+				new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+			});
 		});
-	});
+
+	// getResource('http://localhost:3000/menu')
+	// .then(data => {
+	// 	data.forEach(({img, altimg, title, descr, price}) => {
+	// 		new MenuCard(img, altimg, title, descr, price,'.menu .container').render();
+	// 	});
+	// });
 
 	// getResource('http://localhost:3000/menu')
 	// 	.then(data => createCard(data));
